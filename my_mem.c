6974ba26896b7b9061fc839a7ff260dee99ba297
Juliet Nasar <jnasar@mail.yu.edu>
2022-01-04 14:42:37 -0500

    working fillExp function

--- my_mem.c
+++ my_mem.c
@@ -1,24 +1,26 @@
 # include <stdio.h>
-# include <math.h>
 
 
 /*----------------------------------------------------
 ------------------- UTIL FUNCTIONS -------------------
 ----------------------------------------------------*/
 
+int myPow(int base, int exp){
+    int ans = 1;
+    for(int i = 0; i < exp; i++){
+        ans = ans*base;
+    }
+    return ans;
+}
+
 // returns array of exponents with specified base
 // and exponent range
-int * getExpArr(float base, float expStart, float expEnd){
-    // instantiate 
+int fillExp(int arr[], int base, int expStart, int expEnd){
     int size = expEnd - expStart;
-    int [] expArr = [size];
-
     // go through exponents & add to arr 
-    for(int i = expStart; i <= expEnd; i++){
-        expArr[expStart - i] = pow(base, i);
+    for(int i = 0; i <= size; i++){
+        arr[i] = myPow(base, expStart + i);
     }
-    // arrays return pointers auto so don't need to specify pointer
-    return expArr;
 }
 
 
@@ -35,26 +37,29 @@ void mem_init(unsigned char *my_memory, unsigned int my_mem_size){
     // create a stack with pointers to different sizes
 
     // get array of sizes for our stacks
-    // base = 2.0, expStart = 2.0, expEnd = 12.0
-    int [] expArr = getExpArr(2.0, 2.0, 12.0); 
+    // base = 2, expStart = 2, expEnd = 11
+    static int expArr[10];
+    fillExp(expArr, 2, 2, 11); 
 
-    return 0;
+    /*for(int i = 0; i < 10; i++){
+        printf("%d\n", expArr[i]);
+    }*/
+
 }
 
-/* a function functionally equivalent to malloc() , but allocates it from the memory pool passed to mem_init() */
+/*
+//a function functionally equivalent to malloc() , but allocates it from the memory pool passed to mem_init() 
 void *my_malloc(unsigned size){
-    return 0;
 }
 
-/* a function equivalent to free() , but returns the memory to the pool passed to mem_init()*/
+// a function equivalent to free() , but returns the memory to the pool passed to mem_init()
 void my_free(void *mem_pointer){
-    return 0;
 }
-/*provides statistics about the current allocation of the memory pool.*/
+// provides statistics about the current allocation of the memory pool.
 void mem_get_stats(mem_stats_ptr mem_stats_ptr){
-    return 0;
 }
-
+*/
+/*
 void print_stats(char *prefix) {
   mem_stats_struct mem_stats;
 
@@ -67,16 +72,23 @@ void print_stats(char *prefix) {
 	 mem_stats.largest_block_free,
 	 mem_stats.smallest_block_used,
 	 mem_stats.largest_block_used);
-}
+} 
+*/
+
+
+
 
 int main(int argc, char **argv)
 {
+
     unsigned int global_mem_size = 1024 * 1024;
     unsigned char *global_memory = malloc(global_mem_size);
 
     mem_init(global_memory, global_mem_size);
-    print_stats("init");
+    
+    //print_stats("init");
 
+    /*
     unsigned char *ptr_array[10];
     unsigned int sizes[] = {50, 20, 20, 20, 50, 0};
 
@@ -102,5 +114,7 @@ int main(int argc, char **argv)
     print_stats("after free #0");
 
     my_free(ptr_array[4]);
-    print_stats("after free #4");
-}
+    print_stats("after free #4"); */
+    
+} 
+
