a1e7bea2128d909c4dde6f509d2e0fc52c5db8d1
Juliet Nasar <jnasar@mail.yu.edu>
2022-01-05 12:50:46 -0500

    added mem_stats_struc & get_mem_stats func

--- my_mem.c
+++ my_mem.c
@@ -202,26 +202,118 @@ void my_free(void *mem_pointer){
     }
 
 }
+
+
+// structure for memory location blocks
+typedef struct mem_stats mem_stats_struct;
+struct mem_stats {
+    int num_blocks_free;
+	int num_blocks_used;
+	int smallest_block_free;
+	int largest_block_free;
+	int smallest_block_used;
+	int largest_block_used;
+};
+
 /*
-// provides statistics about the current allocation of the memory pool.
-void mem_get_stats(mem_stats_ptr mem_stats_ptr){
+// function to create a Block
+mem_stats_struct *createMemStatsStruct(int start_loc, int size, char status, Block *prev, Block *next){
+    
+    Block* newBlock;
+
+    newBlock = (Block *)malloc(sizeOf(newBlock));
+
+    newBlock->start_loc = start_loc;
+    newBlock->size = size;
+    newBlock->status = status;
+    newBlock->prev = prev;
+    newBlock->next = next;
+
+    return newBlock;
 }
 */
-/*
-void print_stats(char *prefix) {
-  mem_stats_struct mem_stats;
 
-  mem_get_stats(&mem_stats);
-  printf("mem stats: %s: %d free blocks, %d used blocks, free blocks: smallest=%d largest=%d, used blocks: smallest=%d largest=%d\n",
-	 prefix,
-	 mem_stats.num_blocks_free,
-	 mem_stats.num_blocks_used,
-	 mem_stats.smallest_block_free,
-	 mem_stats.largest_block_free,
-	 mem_stats.smallest_block_used,
-	 mem_stats.largest_block_used);
+// provides statistics about the current allocation of the memory pool.
+void mem_get_stats(mem_stats_struct *mem_stats){
+
+    // first sum vars to 0
+    int sum_free = 0;
+    int sum_used = 0;
+
+    // set to NULL
+    Block *largest_free = NULL;
+    Block *largest_alloc = NULL;
+    Block *smallest_free = NULL;
+    Block *smallest_alloc = NULL;
+
+    // go through linked list starting with head
+    Block *b = head;
+
+    while(b != NULL){
+
+        // increase sum stats
+        char status = b->status;
+        char size = b->size;
+        if(status == 'f'){
+            sum_free += 1; // increase sum free blocks
+
+            // check if larger than largest free
+            if(largest_free == NULL || size > largest_free.size){
+                largest_free = b;
+            }
+            // check if smaller than smallest free
+            if(smallest_free == NULL || size < largest_free.size){
+                smallest_free = b;
+            }
+
+        }
+        if(status == 'a'){
+            sum_used += 1; // increase sum of used blocks
+
+            // check if larger than largest free
+            if(largest_alloc == NULL || size > largest_alloc.size){
+                largest_free = b;
+            }
+            // check if smaller than smallest alloc
+            if(smallest_alloc == NULL || size < largest_alloc.size){
+                smallest_alloc = b;
+            }
+
+        }
+
+       
+
+
+    }
+
+     // set stats to inputted mem_stats_struc
+     mem_stats->num_blocks_free = sum_free;
+     mem_stats->num_blocks_used = sum_used;
+     mem_stats->largest_block_free = largest_free;
+     mem_stats->largest_block_used = largest_alloc;
+     mem_stats->smallest_block_free = smallest_free;
+     mem_stats->smallest_block_used = smallest_alloc;
+
+}
+
+
+void print_stats(char *prefix) {
+
+
+
+    mem_stats_struct mem_stats;
+
+    mem_get_stats(&mem_stats);
+    printf("mem stats: %s: %d free blocks, %d used blocks, free blocks: smallest=%d largest=%d, used blocks: smallest=%d largest=%d\n",
+	    prefix,
+	    mem_stats.num_blocks_free,
+	    mem_stats.num_blocks_used,
+	    mem_stats.smallest_block_free,
+	    mem_stats.largest_block_free,
+	    mem_stats.smallest_block_used,
+	    mem_stats.largest_block_used);
 } 
-*/
+
 
 
 int main(int argc, char **argv)
@@ -232,9 +324,9 @@ int main(int argc, char **argv)
 
     mem_init(global_memory, global_mem_size);
     
-    //print_stats("init");
+    print_stats("init");
 
-    /*
+    
     unsigned char *ptr_array[10];
     unsigned int sizes[] = {50, 20, 20, 20, 50, 0};
 
@@ -260,7 +352,7 @@ int main(int argc, char **argv)
     print_stats("after free #0");
 
     my_free(ptr_array[4]);
-    print_stats("after free #4"); */
+    print_stats("after free #4"); 
     
 } 
 
